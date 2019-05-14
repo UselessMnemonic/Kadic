16c194f84da2de3e20f8fb1997c4dc50981d920d
nihil <c4madrig@ucsd.edu>
2019-05-14 16:03:29 -0700

    Version 1.0 of Kadic

--- kadic/kadicView.m
+++ kadic/kadicView.m
@@ -11,31 +11,37 @@
 
 @implementation kadicView
 
+//these are the RGB components of the hue below
 NSColor *components;
 NSColor *hue;
 
+//these are the bg images for the device
 NSImage *nightImage;
 NSImage *eveningImage;
 NSImage *dayImage;
 
 - (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview {
-  //find the images
+  
+  //find the images in the bundle... must use package name, as main bundle seems to be Preferences
   NSBundle *thisBundle = [NSBundle bundleWithIdentifier:@"nihil.carthage.kadic"];
+  
   NSString *pathToNightImage = [thisBundle pathForResource:@"night-bg" ofType:@"png"];
   NSString *pathToEveningImage = [thisBundle pathForResource:@"evening-bg" ofType:@"png"];
   NSString *pathToDayImage = [thisBundle pathForResource:@"day-bg" ofType:@"png"];
   
-  //load the images
+  //loads the images
   nightImage = [[NSImage alloc] initWithContentsOfFile:pathToNightImage];
   eveningImage = [[NSImage alloc] initWithContentsOfFile:pathToEveningImage];
   dayImage = [[NSImage alloc] initWithContentsOfFile:pathToDayImage];
   
+  //if any of the images aren't loaded, panic
   if( !nightImage || !eveningImage || !dayImage ) return nil;
   
-  //calculate the proper fill-to-screen size
+  //calculate the proper fill-to-screen size here
   NSSize newSize;
   double ratio;
   
+  //choose the larger of the two dimensions and scale to fit
   if( frame.size.width > frame.size.height ) {
     ratio = (double)frame.size.width / nightImage.size.width;
   }
@@ -46,16 +52,14 @@ NSImage *dayImage;
   newSize.height = nightImage.size.height * ratio;
   newSize.width = nightImage.size.width * ratio;
 
-  //set the image sizes
+  //resize the images... horribly inefficient
   [nightImage setSize:newSize];
   [eveningImage setSize:newSize];
   [dayImage setSize:newSize];
-
-  self = [super initWithFrame:frame isPreview:isPreview];
-
-  if( !self ) return nil;
   
-  [self setAnimationTimeInterval:60];
+  self = [super initWithFrame:frame isPreview:isPreview];
+  if( !self ) return nil;
+  [self setAnimationTimeInterval:60]; //update the image every minute
   return self;
 }
 
@@ -70,32 +74,29 @@ NSImage *dayImage;
 - (void)drawRect:(NSRect)rect {
   [super drawRect:rect];
   
-  //reset the rectangle with a black BG
-  [[NSColor blackColor] set];
-  NSRectFill(rect);
-  
   //overlay the images in proper combination
-  if( [components redComponent] > 0.000001 ) 
+  if( [components redComponent] > 0.0001 ) 
       [nightImage drawInRect:rect fromRect:rect operation:NSCompositingOperationSourceOver fraction:[components redComponent]];
   
-  if( [components greenComponent] > 0.000001 ) 
+  if( [components greenComponent] > 0.0001 ) 
       [dayImage drawInRect:rect fromRect:rect operation:NSCompositingOperationSourceOver fraction:[components greenComponent]];
   
-  if( [components blueComponent] > 0.000001 ) 
+  if( [components blueComponent] > 0.0001 ) 
       [eveningImage drawInRect:rect fromRect:rect operation:NSCompositingOperationSourceOver fraction:[components blueComponent]];
 }
 
 - (void) animateOneFrame {
   
   //begin by finding the current progression of the day
-  NSDate *now = [NSDate date];
-  NSCalendar *thisCalendar = [NSCalendar currentCalendar];
-  double progress = ( [thisCalendar component:NSCalendarUnitMinute fromDate:now] / 59.0 );
+  NSDate *now = [NSDate date]; //the current time
+  NSCalendar *thisCalendar = [NSCalendar currentCalendar]; //needed to obtain the units below
+  double progress = ( [thisCalendar component:NSCalendarUnitMinute fromDate:now] / 59.0 ); //using the range [0, 60) -> [0, 360)
+  
   //now generate a mixing mode based on three channels: RGB, using HSB as the hue rotator
   NSColorSpace *csRGBA = [NSColorSpace sRGBColorSpace];
   hue = [NSColor colorWithHue:progress saturation:1.0 brightness:1.0 alpha:1.0];
   components = [hue colorUsingColorSpace:csRGBA];
-  [self setNeedsDisplay:true];
+  [self setNeedsDisplay:true]; //draw function after this call
   return;
 }
 
